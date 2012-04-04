6bd07fc198585ea23aa7c81d97ce750d1d45c135
makevoid <makevoid@gmail.com>
2012-04-04 17:49:25 +0200

    testing in prod

--- lib/fb_comm.rb
+++ lib/fb_comm.rb
@@ -7,16 +7,15 @@ class FBComm
 
   def fetch
     datas = get_full
+    p datas
     datas.map do |post, comments|
       # puts post
       comments = comments["data"]
 
-      puts "-"*80
-      p comments
-
       comments.map do |comment|
         comment = comment.symbolize_keys
         comment[:created_time] = Time.parse comment[:created_time]
+        p comment
         insert_comment_if_new comment, post
       end if comments
     end
